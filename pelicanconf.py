b6a928681dd4837622d0b827fbd88622164fda34
Guru Das Srinagesh <gurooodas@gmail.com>
2023-04-12 00:17:41 -0700

    gurudas: Initial draft of website
    Initial draft of website.

---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+AUTHOR = 'Guru Das Srinagesh'
+SITENAME = 'Guru Das Srinagesh'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'US/Pacific'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+THEME = "theme/gurudas"
+DISPLAY_PAGES_ON_MENU = True
+
+# Prevent generation of files we don't need.
+ARCHIVES_SAVE_AS=''
+AUTHORS_SAVE_AS=''
+CATEGORIES_SAVE_AS=''
+TAGS_SAVE_AS=''
