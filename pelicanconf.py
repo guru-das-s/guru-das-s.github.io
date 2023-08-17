0dec3f032c2a85d7ca2e41e6b25f1c567910e462
Guru Das Srinagesh <gurooodas@gmail.com>
2023-08-16 22:25:39 -0700

    gurudas: Remove DEFAULT_DATE 'fs'
    It's screwing up the last modified dates in the sitemap.
    
    Signed-off-by: Guru Das Srinagesh <gurooodas@gmail.com>

--- pelicanconf.py
+++ pelicanconf.py
@@ -41,7 +41,6 @@ PAGE_SAVE_AS = '{slug}/index.html'
 DRAFT_SAVE_AS=''
 DRAFT_PAGE_SAVE_AS=''
 
-DEFAULT_DATE = 'fs' # Automatically get date from article's filesystem data
 DEFAULT_DATE_FORMAT = '%d %b %Y'
 PAGE_ORDER_BY = 'date'
 
