88035ff4daff3bcb8fba97915d9195d9de461b3b
Guru Das Srinagesh <gurooodas@gmail.com>
2023-11-14 00:35:25 -0800

    gurudas: Remove irrelevant js
    The navbar items all suddenly get aligned when scrolling down the page
    on mobile. Turns out this irrelevant crap is to blame.
    
    Signed-off-by: Guru Das Srinagesh <gurooodas@gmail.com>

--- theme/gurudas/static/js/site.js
+++ theme/gurudas/static/js/site.js
@@ -18,26 +18,10 @@ $(document).ready(function() {
       }
 
   function init() {
-    $window.on('scroll', onScroll)
-    $window.on('resize', resize)
     $popoverLink.on('click', openPopover)
     $document.on('click', closePopover)
     $('a[href^="#"]').on('click', smoothScroll)
     buildSnippets();
-  }
-
-  function smoothScroll(e) {
-    e.preventDefault();
-    $(document).off("scroll");
-    var target = this.hash,
-        menu = target;
-    $target = $(target);
-    $('html, body').stop().animate({
-        'scrollTop': $target.offset().top-40
-    }, 0, 'swing', function () {
-        window.location.hash = target;
-        $(document).on("scroll", onScroll);
-    });
   }
 
   function openPopover(e) {
@@ -59,21 +43,6 @@ $(document).ready(function() {
         scrollTop: $("#elementtoScrollToID").offset().top
     }, 2000);
 });
-
-  function resize() {
-    $body.removeClass('has-docked-nav')
-    navOffsetTop = $nav.offset().top
-    onScroll()
-  }
-
-  function onScroll() {
-    if(navOffsetTop < $window.scrollTop() && !$body.hasClass('has-docked-nav')) {
-      $body.addClass('has-docked-nav')
-    }
-    if(navOffsetTop > $window.scrollTop() && $body.hasClass('has-docked-nav')) {
-      $body.removeClass('has-docked-nav')
-    }
-  }
 
   function escapeHtml(string) {
     return String(string).replace(/[&<>"'\/]/g, function (s) {
